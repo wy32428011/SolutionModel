de8b796de74309b15b9a2d3d4c3c167d5a4a5335
WangYun <didi32428011@sina.com>
2015-08-07 20:47:26 +0800

    Core_BaseEntity

---
+++ SolutionModel.Core/BaseEntity.cs
@@ -0,0 +1,77 @@
+﻿/* ---------------------------------------------------------------------    
+ * Copyright:
+ * Wuxi Efficient Technology Co., Ltd. All rights reserved. 
+ * 
+ * Class Description:
+ * 
+ *
+ * Comment 					        Revision	Date        Author
+ * -----------------------------    --------    --------    -----------
+ * Created							1.0		    2015/8/7 20:37:54 vincent@xcloudbiz.com
+ *
+ * ------------------------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionModel.Core
+{
+    /// <summary>
+    /// 基础实体类
+    /// </summary>
+    public abstract partial class BaseEntity
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj as BaseEntity);
+        }
+        private static bool IsTransient(BaseEntity obj)
+        {
+            return obj != null && Equals(obj.Id, default(int));
+        }
+        private Type GetUnproxiedType()
+        {
+            return GetType();
+        }
+        public virtual bool Equals(BaseEntity other)
+        {
+            if (other == null)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            if(!IsTransient(this) && 
+               !IsTransient(other) &&
+               Equals(Id, other.Id))
+            {
+                var otherType = other.GetUnproxiedType();
+                var thisType = GetUnproxiedType();
+                return thisType.IsAssignableFrom(otherType) ||
+                    otherType.IsAssignableFrom(thisType);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            if(Equals(Id, default(int)))
+                return base.GetHashCode();
+            return Id.GetHashCode();
+        }
+
+        public static bool operator == (BaseEntity x, BaseEntity y)
+        {
+            return Equals(x, y);
+        }
+        public static bool operator !=(BaseEntity x, BaseEntity y)
+        {
+            return !(x == y);
+        }
+    }
+}
