058e75b0aa50fabec95fdc0eede840460d4a62fb
WangYun <didi32428011@sina.com>
2015-08-09 00:53:15 +0800

    安装包

---
+++ SolutionModel.Data/IDbContext.cs
@@ -0,0 +1,26 @@
+﻿/* ---------------------------------------------------------------------    
+ * Copyright:
+ * Wuxi Efficient Technology Co., Ltd. All rights reserved. 
+ * 
+ * Class Description:
+ * 
+ *
+ * Comment 					        Revision	Date        Author
+ * -----------------------------    --------    --------    -----------
+ * Created							1.0		    2015/8/7 20:50:43 vincent@xcloudbiz.com
+ *
+ * ------------------------------------------------------------------------------*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SolutionModel.Data
+{
+    public interface IDbContext
+    {
+        //IDbSet<T>
+    }
+}
